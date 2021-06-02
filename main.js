7cad15ac4598aa32f9a75ab89a0ddd7bef8d5600
mayrant <mayrant2004@yahoo.com>
2021-06-02 01:41:43 -0500

    unit tests working

--- main.js
+++ main.js
@@ -30,28 +30,57 @@ const printStacks = () => {
 }
 
 // Next, what do you think this function should do?
-const movePiece = () => {
-  // Your code here
-
+// -- move from one key to another - startStack to endStack
+// -- pop from the startStack, push to the endStack
+const movePiece = (startStack, endStack) => {
+  if (isLegal(startStack, endStack)) {
+    stacks[endStack].push(stacks[startStack].pop())
+  }
 }
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-  // Your code here
+// -- the startStack has to be less than the endStack for the move to be legal (can't put a bigger block on top of smaller black)
+const isLegal = (startStack, endStack) => {
+  // take in parameters - check
+  // if endStack length is 0
+    // return true
+  // if endStack length is not 0
+    // get last element of endStack (pop? [arr.length - 1]?)
+    // if last item of endStack is less than startStack item to be moved
+      // return false
+    // else
+      // return true 
+  let moveItem = stacks[startStack] - 1;
+  let lastItem = stacks[endStack] - 1;
 
-}
+  if (stacks[endStack].length === 0) {
+    return true;
+  } else if ((stacks[endStack].length > 0) && (moveItem < lastItem)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // What is a win in Towers of Hanoi? When should this function run?
+// -- if stack b or c is 4 numbers long, it's a win
+// maybe loop? let win = [4,3,2,1]?
 const checkForWin = () => {
-  // Your code here
-
-}
+  if ((stacks.b.length === 4) || (stacks.c.length === 4)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // When is this function called? What should it do with its argument?
+// movePiece(startStack, endStack);
 const towersOfHanoi = (startStack, endStack) => {
-  // Your code here
-
-}
+  movePiece(startStack, endStack);
+  if (checkForWin()) {
+    alert('You won!');
+  }
+};
 
 const getPrompt = () => {
   printStacks();
---
+++ myREADME.md
@@ -0,0 +1,7 @@
+1. 20pts - **Code Plan** - Include this in a `README.md` file in your folder with comments in your code
+
+- What needs to happen?
+- 1. Blocks need to be moved in a certain fashion.
+- - a. Move last item from starting array to ending array (pop & push)
+- - b. The starting value needs to be greater than the ending value
+- 2. There is a way to win the game.
